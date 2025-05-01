512ece5b1c15fc349c4dd559c2f0164820409cd1
CarlosDanielGoCaza <gonzalezcazarescarlosdaniel@gmail.com>
2025-05-01 11:24:36 -0600

    CarlosDanielGoCaza updated all project

--- src/main/main.js
+++ src/main/main.js
@@ -1,29 +1,31 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  const win = new BrowserWindow({
+    fullscreenable: true,
+    width,
+    height,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-    win.loadFile('../renderer/views/index.html');
+  win.loadFile('../renderer/views/index.html');
 }
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow();
 
-app.on('activate', () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
