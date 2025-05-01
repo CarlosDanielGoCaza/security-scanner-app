accf642544725e284f72b2c59636f8a8081a2b78
CarlosDanielGoCaza <gonzalezcazarescarlosdaniel@gmail.com>
2025-05-01 12:56:45 -0600

    CarlosDanielGoCaza added a bridge from index.html to registrarusuario.html

--- src/renderer/js/index.js
+++ src/renderer/js/index.js
@@ -1,5 +1,11 @@
 const { ipcRenderer } = require('electron');
 
+
 document.getElementById('login-card-acceder').addEventListener('click', () => {
   ipcRenderer.send('navigate', 'acceder'); // carga acceder.html
 });
+
+document.getElementById('login-card-registro').addEventListener('click', () => {
+    ipcRenderer.send('navigate', 'registrarusuario'); // carga registrarusuario.html
+  });
+  
