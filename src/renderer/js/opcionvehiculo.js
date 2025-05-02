698dc5611e4d36a664bc07b667832ff8d6ef961d
ArmandoSA26 <94924052+ArmandoSA26@users.noreply.github.com>
2025-05-01 23:08:21 -0600

    Startik Added errorUsuariVehiculo and asociarplacasvehiculo.js

--- src/renderer/js/opcionvehiculo.js
+++ src/renderer/js/opcionvehiculo.js
@@ -4,3 +4,7 @@ const { ipcRenderer } = require('electron');
 document.getElementById('asociar-placas').addEventListener('click', () => {
   ipcRenderer.send('navigate', 'asociarplacasvehiculo'); // carga acceder.html
 });
+
+document.getElementById('btn-volver-accs').addEventListener('click', () => {
+  ipcRenderer.send('navigate', 'acceder'); // carga acceder.html
+});
